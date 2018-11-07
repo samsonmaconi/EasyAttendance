63ea401748b47fe8227b2e518206bc83e43558fd
Pam1024 <lan.chen@dal.ca>
2018-11-06 22:26:10 -0400

    welcome functionality update 1

--- app/src/main/java/com/example/navkaran/easyattendance/WelcomeActivity.java
+++ app/src/main/java/com/example/navkaran/easyattendance/WelcomeActivity.java
@@ -2,12 +2,20 @@ package com.example.navkaran.easyattendance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    public EditText et_userId;
+    public Button bt_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        et_userId = findViewById(R.id.et_userId);
+        bt_save = findViewById(R.id.bt_save);
     }
 }
