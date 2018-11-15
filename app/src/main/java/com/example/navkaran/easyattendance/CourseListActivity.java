a2e1fdf9101998e1d0566ead63d183e142394157
davidcui5 <yq506499@dal.ca>
2018-11-15 01:36:13 -0400

    changed intent

--- app/src/main/java/com/example/navkaran/easyattendance/CourseListActivity.java
+++ app/src/main/java/com/example/navkaran/easyattendance/CourseListActivity.java
@@ -1,14 +1,23 @@
 package com.example.navkaran.easyattendance;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 // David Cui B00788648 Nov 2018
 //activity for course list, where instructors can select the course they want to enable check-in for
 public class CourseListActivity extends AppCompatActivity {
+
+    //for accessing extras
+    public static final String COURSE_ID = "COURSE_ID";
+    public static final String COURSE_NAME = "COURSE_NAME";
+    public static final String COURSE_STUDENT_COUNT = "COURSE_STUDENT_COUNT";
 
     private CourseAdapter adapter;
     //the list of courses this instructor is teaching
@@ -30,6 +39,23 @@ public class CourseListActivity extends AppCompatActivity {
         lvCourseList.setAdapter(adapter);
 
         populateCourseList();
+
+        //TODO
+        // get extra from intent (instructor id)
+        // get course list from file or database instead of hard coding
+
+        lvCourseList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent();
+                intent.setClass(view.getContext(), TakeAttendanceActivity.class);
+                CourseItem course = courseItemArrayList.get(i);
+                intent.putExtra(COURSE_ID, course.getCourseID());
+                intent.putExtra(COURSE_NAME, course.getCourseName());
+                intent.putExtra(COURSE_STUDENT_COUNT, course.getStudentCount());
+                startActivity(intent);
+            }
+        });
     }
 
     //fake data for the sake of trying out the layout
