8060f05d32efdc17d9feb0682daa251658063d20
Shengtian Tang <tsttbx@live.com>
2018-11-20 21:14:44 -0400

    new start attendance method

--- app/src/main/java/com/example/navkaran/easyattendance/TakeAttendanceActivity.java
+++ app/src/main/java/com/example/navkaran/easyattendance/TakeAttendanceActivity.java
@@ -1,8 +1,12 @@
 package com.example.navkaran.easyattendance;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +17,9 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -39,7 +46,14 @@ public class TakeAttendanceActivity extends AppCompatActivity {
     private int student_num;
     private Runnable runnable;
     private Handler handler;
-    private String attendanceCount;
+    private int attendanceCount;
+    private boolean shouldStop = false;
+    private boolean shouldCheck = false;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private String location_error = "no error";
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +76,7 @@ public class TakeAttendanceActivity extends AppCompatActivity {
 
         class_number.setText(course_id);
         class_name.setText(course_name);
+        check_number.setText("not yet started");
         register_number.setText(student_num+" Students Registered");
 
         handler = new Handler();
@@ -71,7 +86,9 @@ public class TakeAttendanceActivity extends AppCompatActivity {
                 runnable = new Runnable() {
                     @Override
                     public void run() {
-                        checkAttendance();
+                        if(shouldCheck){
+                            checkAttendance();
+                        }
                     }
                 };
                 Thread thread = new Thread(null, runnable, "background");
@@ -80,6 +97,7 @@ public class TakeAttendanceActivity extends AppCompatActivity {
             }
         }, 0);
 
+        setLocation();
     }
 
 
@@ -87,13 +105,20 @@ public class TakeAttendanceActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
-            handler.removeCallbacksAndMessages(null);
-
-            startNewIntend();
+            if(shouldStop){
+                handler.removeCallbacksAndMessages(null);
+                stopAttendance();
+            }else {
+                startAttendance(course_id,course_name,longitude,latitude);
+                stop_btn.setText(R.string.action_stop_attendance);
+                stop_btn.setBackgroundResource(R.drawable.round_button_red_selector);
+                shouldCheck = true;
+                shouldStop = true;
+            }
         }
     };
 
-    private void startNewIntend(){
+    private void stopAttendance(){
         Intent intent = new Intent(this, AttendanceDetailsActivity.class);
         intent.putExtra(COURSE_ID, course_id);
         intent.putExtra(COURSE_NAME, course_name);
@@ -111,7 +136,7 @@ public class TakeAttendanceActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            attendanceCount = response.getString("students_count");
+                            attendanceCount = response.getInt("students_count");
                             check_number.setText(attendanceCount+" of "+student_num+" in attendance");
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -127,5 +152,55 @@ public class TakeAttendanceActivity extends AppCompatActivity {
         }
         );
         RequestQueueSingleton.getmInstance(getApplicationContext()).addToRequestQueue(request);
+    }
+
+    private void startAttendance(String course_id, String course_name, double lon, double lat){
+        final String url = "https://web.cs.dal.ca/~stang/csci5708/start_attendance.php?class_info="+course_id+","+course_name+","+lon+","+lat;
+        System.out.println(url);
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Toast.makeText(getApplicationContext(),"CourseListActivity Error",Toast.LENGTH_SHORT).show();
+
+            }
+        }
+        );
+        RequestQueueSingleton.getmInstance(getApplicationContext()).addToRequestQueue(request);
+    }
+
+    private void setLocation(){
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    // Got last known location. In some rare situations this can be null.
+                    if (location != null) {
+                        // save longitude and latitude to a local variable for future use
+                        longitude = location.getLongitude();
+                        latitude = location.getLatitude();
+                        System.out.println("************CourseListActivity************** longitude: "+longitude+"    latitude: "+latitude);
+                    }else {
+                        location_error = "Unknown Location";
+                        System.out.println("**************** "+location_error);
+                    }
+                }
+            });
+            return;
+        }else{
+            location_error = "Permission Denied";
+            System.out.println("**************** "+location_error);
+        }
     }
 }
