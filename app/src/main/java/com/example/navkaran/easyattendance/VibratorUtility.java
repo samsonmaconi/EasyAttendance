3a1eaa59eae6a298643be2540c86ba5427603615
Samson Maconi <nivlek_000@yahoo.com>
2018-11-23 04:20:48 -0400

    Added Two New Utility Classes -VibratorUtility for device Vibrations -DistanceChecker to compare coordinate distance -Manifest uses VIBRATE permission -Added parent attributes for Manifest Activities

---
+++ app/src/main/java/com/example/navkaran/easyattendance/VibratorUtility.java
@@ -0,0 +1,54 @@
+package com.example.navkaran.easyattendance;
+/**
+ * This is a Singleton Utility Class for
+ * initiating device vibrations. It is instantiated
+ * at the first call of it's static vibrate method.
+ */
+
+import android.content.Context;
+import android.os.Vibrator;
+import android.util.Log;
+
+public class VibratorUtility {
+    private static String TAG = "VibratorUtility";
+    private static Vibrator vibrator;
+    private static VibratorUtility vibratorUtility;
+
+
+    private VibratorUtility(Context context){
+        vibrator = (Vibrator) context.getSystemService(context.VIBRATOR_SERVICE);
+    }
+
+    public static void vibrate(final Context context, final int duration, final int count) {
+        if(vibratorUtility==null){
+            vibratorUtility = new VibratorUtility(context);
+        }
+
+        if (vibratorUtility.vibrator.hasVibrator()) {
+            Log.d(TAG, "Has Vibrator");
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int c = count;
+                    while (c-- > 0) {
+                        exec(context, duration);
+                        try {
+                            Thread.sleep(200);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+            }).start();
+
+        } else {
+            Log.d(TAG, "No Vibrator");
+        }
+    }
+
+    private static void exec(Context context, int duration) {
+        vibratorUtility.vibrator.vibrate(duration);
+    }
+
+}
