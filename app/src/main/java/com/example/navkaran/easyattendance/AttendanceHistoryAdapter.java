3eeffc742ef97492a088a39bad510fbafbdcd572
Navkaran0105 <navkarank@gmail.com>
2018-11-23 01:18:13 -0400

    Auto updation of Course info in history

--- app/src/main/java/com/example/navkaran/easyattendance/AttendanceHistoryAdapter.java
+++ app/src/main/java/com/example/navkaran/easyattendance/AttendanceHistoryAdapter.java
@@ -1,4 +1,48 @@
 package com.example.navkaran.easyattendance;
 
-public class AttendanceHistoryAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class AttendanceHistoryAdapter extends ArrayAdapter<Lecture> {
+    private List<Lecture> LectureList;
+
+    public AttendanceHistoryAdapter(Context context, int resource, List<Lecture> LectureList) {
+        super(context, resource, LectureList);
+        this.LectureList = LectureList;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+
+        // check to see if view is null. If it is, inflate it
+        // inflating means to show the view
+        if (v == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(R.layout.attendance_history_list_item, null);
+        }
+
+        Lecture i = LectureList.get(position);
+
+        if (i != null) {
+            TextView attenDate = v.findViewById(R.id.attendate);
+            //TextView attenDay = v.findViewById(R.id.attenday);
+            TextView attenCount = v.findViewById(R.id.attenCount);
+
+            attenDate.setText(i.getDate().toString());
+            //attenDay.setText();
+            attenCount.setText(String.valueOf(i.getNumAttendee()));
+        }
+        return v;
+    }
+
 }
